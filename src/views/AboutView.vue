2389d2aa5513563579aa3535c0c037a26608d29a
Muhammad Yusuf Habibie <myh481813@gmail.com>
2024-07-14 11:39:13 +0700

    add: about & profil view

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,11 +1,88 @@
 <script setup>
 import NavbarComponent from '@/components/NavbarComponent.vue';
+import { useRouter, RouterView, useRoute } from 'vue-router';
+
+const route = useRoute();
+const router = useRouter();
+console.log(router.getRoutes);
+
+function showProfile() {
+  router.push({name: "profile"});
+}
+function showHistory() {
+  router.push({name: "history"});
+}
+
+
 </script>
 
 <template>
-  <NavbarComponent/>
+  <NavbarComponent />
+  <main>
+    <div class="container container-custom">
+      <div class="about">
+        <h1>Nama Pengguna</h1>
+        <input @click="showProfile" type="button" value="Akun Saya" :class="{ active: route.fullPath == '/about/profile'}">
+        <input @click="showHistory" type="button" value="Riwayat Pinjam" :class="{ active: route.fullPath == '/about/history'}">
+        <input type="button" value="Keluar">
+      </div>
+      <div class="dinamis">
+        <RouterView/>
+      </div>
+    </div>
+  </main>
 </template>
 
 <style scoped>
+main {
+  background-color: #EEEBE1;
+  min-height: 100vh;
+}
+.container-custom {
+  display: flex;
+  /* border: 1px black solid; */
+  justify-content: space-between;
+}
+.about {
+  margin-top: 200px;
+  border: 3px black solid;
+  display: flex;
+  flex-direction: column;
+  height: 330px;
+  background-color: white;
+  justify-content: space-evenly;
+  align-items: center;
+  padding: 10px 20px;
+  border-radius: 20px;
+  box-shadow: 10px 10px 20px black;
+  position: fixed;
+}
+.about h1 {
+  font-size: 2em;
+}
 
+.about input {
+  background-color: #E9E9E9;
+  width: 270px;
+  height: 50px;
+  border-radius: 20px;
+  border: 0;
+}
+
+.dinamis {
+  width: 950px;
+  border: 3px solid black;
+  border-radius: 20px;
+  box-shadow: 10px 10px 20px black;
+  margin-top: 200px;
+  /* height: 100vh; */
+  margin-left: 400px;
+  overflow:hidden;
+}
+
+.about .active {
+  background-color: #8e8e8e;
+  /* color: white; */
+  border: 3px solid black;
+}
 </style>
--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,3 +1,59 @@
 <template>
 
+  <div class="container-fluid profile">
+    <div class="headprofile">
+      <h1>Profile Saya</h1>
+      <p>Kelola informasi profil Anda untuk mengontrol, melindungi dan mengamankan akun</p>
+    </div>
+    <div class="button">
+      <h2>Update username disini</h2>
+      <form action="">
+        <Label for="user">Username :</Label>
+        <input type="text" value="contoh123" name="user" id="user">
+        <input type="button" value="Update username!" id="update">
+      </form>
+    </div>
+  </div>
+
 </template>
+
+<style scoped>
+.profile {
+  background-color: white;
+  padding: 20px;
+}
+
+.button {
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+}
+
+.button h2 {
+  font-size: 2em;
+  margin-bottom: 20px;
+}
+
+.button label {
+  font-size: 1.5em;
+  margin-right: 10px
+}
+
+#user {
+  height: 50px;
+  border-radius: 15px;
+  padding: 10px 10px;
+  font-size: 1em;
+}
+
+#update {
+  display: block;
+  margin-left: 50px;
+  margin-top: 30px;
+  width: 150px;
+  height: 40px;
+  border-radius: 10px;
+  background-color: #F8BC61;
+  margin-bottom: 20px;
+}
+</style>
